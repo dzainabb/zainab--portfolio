585675c958206f6b26d9cb32c08337c97162290d
dzainabb <zainabdar24@yahoo.com>
2025-12-04 23:05:04 +0000

    adding in histogram that shows fraction of practicular candidate - using user input

--- pandas_activity/us_elections.py
+++ pandas_activity/us_elections.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-elections_df = pd.read_csv('/Users/sabiqadar/Downloads/US-2016-primary.csv',sep=";")
+elections_df = pd.read_csv('/Users/sabiqadar/Desktop/zainab-portfolio/pandas_activity/US-2016-primary.csv',sep=";")
 
 
 print(elections_df.head(3))
@@ -29,11 +29,17 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-#votes distribution by party
-plt.figure(figsize=(10,6))
-plt.hist(elections_df['votes'], bins=30, color='salmon', edgecolor='black') 
-plt.xlabel('Votes')
-plt.ylabel('Frequency')     
-plt.title('Votes Distribution in US 2016 Primary Elections')
+#fraction of particular candidate's votes in each state
+#allows user to input candidate name - need to spell correctly as in dataset
+
+candidate_name = input('Enter candidate name: ')
+
+candidate_data = elections_df[elections_df['candidate'] == candidate_name]
+plt.figure(figsize=(12,6))
+plt.bar(candidate_data['state'], candidate_data['fraction_votes'], color='orchid')
+plt.xlabel('State')
+plt.ylabel('Fraction of Votes')
+plt.title(f'Fraction of Votes for {candidate_name} by State in US 2016 Primary Elections')
+plt.xticks(rotation=90)
 plt.tight_layout()
 plt.show()
